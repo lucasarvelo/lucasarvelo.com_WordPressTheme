7ef4575e775f83de4e5a9450069164a606f91e21
lucasarvelo <contact@lucasarvelo.com>
2018-02-02 15:10:46 -0500

    Replace names '-s' to 'lucasarvelo'
    Signed-off-by: lucasarvelo <contact@lucasarvelo.com>

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -14,27 +14,27 @@
 /**
  * Set up the WordPress core custom header feature.
  *
- * @uses _s_header_style()
+ * @uses lucasarvelo_header_style()
  */
-function _s_custom_header_setup() {
-	add_theme_support( 'custom-header', apply_filters( '_s_custom_header_args', array(
+function lucasarvelo_custom_header_setup() {
+	add_theme_support( 'custom-header', apply_filters( 'lucasarvelo_custom_header_args', array(
 		'default-image'          => '',
 		'default-text-color'     => '000000',
 		'width'                  => 1000,
 		'height'                 => 250,
 		'flex-height'            => true,
-		'wp-head-callback'       => '_s_header_style',
+		'wp-head-callback'       => 'lucasarvelo_header_style',
 	) ) );
 }
-add_action( 'after_setup_theme', '_s_custom_header_setup' );
+add_action( 'after_setup_theme', 'lucasarvelo_custom_header_setup' );
 
-if ( ! function_exists( '_s_header_style' ) ) :
+if ( ! function_exists( 'lucasarvelo_header_style' ) ) :
 	/**
 	 * Styles the header image and text displayed on the blog.
 	 *
-	 * @see _s_custom_header_setup().
+	 * @see lucasarvelo_custom_header_setup().
 	 */
-	function _s_header_style() {
+	function lucasarvelo_header_style() {
 		$header_text_color = get_header_textcolor();
 
 		/*
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -16,20 +16,20 @@
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php
-				_s_posted_on();
-				_s_posted_by();
+				lucasarvelo_posted_on();
+				lucasarvelo_posted_by();
 			?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php _s_post_thumbnail(); ?>
+	<?php lucasarvelo_post_thumbnail(); ?>
 
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 
 	<footer class="entry-footer">
-		<?php _s_entry_footer(); ?>
+		<?php lucasarvelo_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
